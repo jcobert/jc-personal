e3207f2ae0b8967a80139a2eee8a6cc817d9bcd5
Josh Cobert <jdcobert@gmail.com>
2025-03-26 01:56:39 -0400

    create strapi fetch client

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,14 @@
 import { Metadata } from 'next'
 import { FC } from 'react'
 
-import { strapiFetch } from '@/lib/strapi/fetch'
-import { StrapiAPIResponse } from '@/lib/strapi/types/types'
+import { strapiClient } from '@/lib/strapi/fetch'
 
 import PageLayout from '@/components/layout/page-layout'
 
 import { buildPageTitle } from '@/configuration/seo'
 
 const loadContent = async () => {
-  const res =
-    await strapiFetch<StrapiAPIResponse<'api::home-page.home-page'>>(
-      '/home-page',
-    )
+  const res = await strapiClient.GET('/home-page')
   return res?.data?.data
 }
 
--- frontend/src/lib/strapi/fetch.ts
+++ frontend/src/lib/strapi/fetch.ts
@@ -1,8 +1,21 @@
-import fetch from '@/utils/fetch'
+import { paths } from './types/api'
+import createClient from 'openapi-fetch'
+import qs from 'qs'
 
-/** Fetches data from the provided Strapi endpoint. */
-export const strapiFetch = <TData>(path: string) => {
-  const baseUrl = process.env.CMS_BASE_URL || ''
-  const url = new URL(`/api${path}`, baseUrl)
-  return fetch.GET<TData>({ url: url.href })
-}
+/** Client to fetch data from Strapi API. */
+export const strapiClient = createClient<paths>({
+  baseUrl: `${process.env.CMS_BASE_URL}/api`,
+  headers: { Accept: 'application/json' },
+  querySerializer: (params) => {
+    return qs.stringify(params, {
+      encodeValuesOnly: true,
+    })
+  },
+})
+
+// /** Fetches data from the provided Strapi endpoint. */
+// export const strapiFetch = <TData>(path: string) => {
+//   const baseUrl = process.env.CMS_BASE_URL || ''
+//   const url = new URL(`/api${path}`, baseUrl)
+//   return fetch.GET<TData>({ url: url.href })
+// }
