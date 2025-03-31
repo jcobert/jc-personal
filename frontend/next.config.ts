cff8ca303b2020de1cb75cde3691d80768e71d09
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2025-03-31 17:28:32 -0400

    Task/update next config images (#6)
    * add image domain
    
    * add volume to fly config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: { taint: true },
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: process.env.NODE_ENV === 'development' ? 'http' : 'https',
+        hostname: process.env.CMS_IMAGE_DOMAIN || '',
+      },
+    ],
     minimumCacheTTL: 31536000,
   },
   rewrites: async () => [
@@ -13,6 +18,11 @@ const nextConfig: NextConfig = {
     { source: '/health', destination: '/api/health' },
     { source: '/ping', destination: '/api/health' },
   ],
+  env: {
+    NEXT_PUBLIC_SITE_BASE_URL:
+      process.env.NEXT_PUBLIC_SITE_BASE_URL ??
+      `https://${process.env.VERCEL_URL}`,
+  },
 }
 
 export default nextConfig
