cb9955a5580409e4c20f2f6703ca015bb898d7ba
Josh Cobert <jdcobert@gmail.com>
2025-04-01 23:30:13 -0400

    update photo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next'
 import { FC } from 'react'
 
 import { getHomePage } from '@/lib/strapi/queries/home-page'
-import { getStrapiImageUrl } from '@/lib/strapi/utils'
 
+import ProfilePhoto from '@/components/features/home-page/profile-photo'
 import PageLayout from '@/components/layout/page-layout'
 
 import { buildPageTitle } from '@/configuration/seo'
@@ -27,14 +27,7 @@ const HomePage: FC = async () => {
         <div className='prose flex flex-col gap-8 max-md:mx-auto'>
           <div className='flex items-center md:items-end max-md:flex-col gap-x-8 gap-y-4'>
             <h1>{title}</h1>
-            <div
-              role='img'
-              aria-label='josh cobert'
-              className='flex-none size-40 mx-auto bg-center bg-cover bg-no-repeat rounded-full shadow-lg border-4 border-gray-5'
-              style={{
-                backgroundImage: `url(${getStrapiImageUrl(profilePhoto?.url)})`,
-              }}
-            />
+            <ProfilePhoto image={profilePhoto} />
           </div>
           <p>{description}</p>
         </div>
