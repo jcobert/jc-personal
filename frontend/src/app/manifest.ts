50631d7e42c0be5465f4348373890a46850bbee3
Josh Cobert <jdcobert@gmail.com>
2025-04-27 01:45:00 -0400

    add icons

---
+++ frontend/src/app/manifest.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+const manifest = (): MetadataRoute.Manifest => ({
+  name: 'Josh Cobert',
+  short_name: 'JC',
+  icons: [
+    {
+      src: '/web-app-manifest-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+    {
+      src: '/web-app-manifest-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'maskable',
+    },
+  ],
+  background_color: '#FDFDFF',
+  theme_color: '#484d92',
+  display: 'standalone',
+})
+
+export default manifest
