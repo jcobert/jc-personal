a94c175e5a174f36607bc6ee4862fe8b38478375
Josh Cobert <jdcobert@gmail.com>
2025-07-09 22:12:29 -0400

    add links

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -1,33 +1,55 @@
+import ContactLink from '../features/contact-page/contact-link'
 import { FC } from 'react'
 import { FaCodeBranch } from 'react-icons/fa'
 
-import { repoLink } from '@/utils/constants'
+import { getContactPage } from '@/lib/strapi/queries/contact-page'
+
+import { CURRENT_YEAR, repoLink } from '@/utils/constants'
 
 import Link from '@/components/general/link'
 
 import { siteConfig } from '@/configuration/site'
 
-const Footer: FC = () => {
-  const currentYear = new Date().getFullYear()
+const Footer: FC = async () => {
+  const { links } = (await getContactPage()) || {}
 
   return (
     <div className='w-full bg-gray-11 pb-safe mt-8 print:hidden'>
       <div aria-hidden className='h-px w-full border-b border-gray-7' />
 
-      <div className='flex max-md:px-4 max-md:flex-col items-center py-8 mx-auto text-white w-full md:layout md:py-2 gap-8 md:justify-between'>
+      <div className='flex max-md:px-4 max-md:flex-col-reverse items-center py-8 mx-auto text-white w-full md:layout md:py-2 gap-8 md:justify-between'>
         <p id='copyright' className='text-xs'>
-          &copy; {`${currentYear} ${siteConfig?.title}`}
+          &copy; {`${CURRENT_YEAR} ${siteConfig?.title}`}
         </p>
 
-        <Link
-          href={repoLink}
-          variant='tertiary'
-          className='text-sm sm:text-xs font-medium text-white no-hover:active:text-gray-4'
-          color='general'
-        >
-          <FaCodeBranch aria-hidden />
-          View source code
-        </Link>
+        <div className='flex items-center gap-x-6 gap-y-4 max-md:flex-col max-md:w-full'>
+          {links?.length ? (
+            <div className='flex items-center max-sm:w-full max-sm:justify-around sm:gap-16 md:gap-4 max-md:py-4'>
+              {links?.map((link) => (
+                <ContactLink
+                  key={link?.id}
+                  link={link}
+                  aria-label={link?.name}
+                  className='[&:not(:disabled)]:hover:bg-transparent hover:text-gray-4 p-1 text-white w-fit'
+                  iconClassName='md:text-2xl'
+                  variant='tertiary'
+                  color='general'
+                  showText={false}
+                />
+              ))}
+            </div>
+          ) : null}
+
+          <Link
+            href={repoLink}
+            variant='tertiary'
+            className='text-sm md:text-xs font-medium text-white no-hover:active:text-gray-4'
+            color='general'
+          >
+            <FaCodeBranch aria-hidden />
+            View source code
+          </Link>
+        </div>
       </div>
     </div>
   )
